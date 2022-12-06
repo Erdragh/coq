1f9fbb82aa0b293c017d6c593d19293de8d60e19
Erdragh <enderjanyt@gmail.com>
2022-12-06 09:56:06 +0100

    exercise 7/3/ab

--- fallunterscheidung.v
+++ fallunterscheidung.v
@@ -20,3 +20,40 @@ Proof.
         exact D.
     contradiction. (*y and ~y*)
 Qed.
+End Fallunterscheidung.
+
+Parameters A B: Prop.
+
+Theorem GD: (A -> B) \/ (B -> A).
+Proof.
+    apply FU with (x := A).
+    split.
+    intro.
+    right.
+    intro.
+    assumption.
+
+    intro.
+    left.
+    intro.
+    contradiction.
+Qed.
+
+Theorem PL: ((A -> B) -> A) -> A.
+Proof.
+    apply FU with (x := A).
+    split.
+        intro C.
+        intro D.
+        exact C.
+        intro C.
+        intro D.
+        assert (A -> B).
+            intro E.
+            contradiction.
+        apply D.
+        exact H.
+Qed.
+
+
+    
