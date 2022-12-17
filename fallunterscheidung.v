c5a954ecfb35fbb1e3311111b707bfe0209f0e1f
Erdragh <enderjanyt@gmail.com>
2022-12-17 14:50:40 +0100

    do exercise 3

--- fallunterscheidung.v
+++ fallunterscheidung.v
@@ -22,6 +22,8 @@ Proof.
 Qed.
 End Fallunterscheidung.
 
+(* ------------- Präsenzaufgabe (Aufgabe 1) -------------*)
+
 Parameters A B: Prop.
 
 Theorem GD: (A -> B) \/ (B -> A).
@@ -65,6 +67,8 @@ Proof.
     right.
     assumption.
 Qed.
+
+(* ---------------- Aufgabe 2 -------------------- *)
 
 (*
 I could probably have used assumption a lot in here,
@@ -153,4 +157,40 @@ Proof.
         split.
         exact yesA.
         exact H.
+Qed.
+
+(* -------------- Aufgabe 3 -------------- *)
+
+Parameters isElephant forgets wonMastermind hasTrunk didMastermind: Prop.
+
+Theorem Exercise3: ((
+    (isElephant -> ~forgets) /\ 
+    (wonMastermind -> ~hasTrunk) /\
+    ((~forgets /\ didMastermind) -> wonMastermind) /\
+    (~hasTrunk -> ~isElephant) /\
+    (isElephant /\ didMastermind)) -> False).
+Proof.
+    intro Conjunction.
+    destruct Conjunction as [A1 A2].
+    destruct A2 as [A2 A3].
+    destruct A3 as [A3 A4].
+    destruct A4 as [A4 A5].
+    destruct A5 as [Elephant Participated].
+    assert (~forgets) as DoesntForget.
+        apply A1.
+        exact Elephant.
+    assert (~forgets /\ didMastermind) as ShouldWinMastermind.
+        split.
+        exact DoesntForget.
+        exact Participated.
+    assert (wonMastermind) as IsWinner.
+        apply A3.
+        exact ShouldWinMastermind.
+    assert (~hasTrunk) as DoesntHaveTrunk.
+        apply A2.
+        exact IsWinner.
+    assert (~isElephant) as NotElephant.
+        apply A4.
+        exact DoesntHaveTrunk.
+    contradiction.
 Qed.
